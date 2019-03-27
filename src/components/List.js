a6e0a4c34ea3a463a785bca25dc50aa8186ffc72
Hidayat arghandabi <hidayatarg@gmail.com>
2019-03-27 14:39:34 +0300

    Completed the List Component

--- src/components/List.js
+++ src/components/List.js
@@ -8,10 +8,23 @@ class List extends Component {
             <div className={"listArea"}>
             <input name={"filter"} id={"filter"} placeholder={"Filter by name or phone"} />
             <ul className={"list"}>
-                <li>Mehmet Yildiz</li>
-                <li>Cem Yilmaz</li>
-                <li>Hidayat Arghandabi</li>
-                <li>Mehmet Seven</li>
+                <li>
+                    <span className={"name"}>Mehmet Yildiz</span>
+                    <span className={"phone"}>111111111111</span>
+                    <span className={"clearfix"}></span>
+                </li>
+                <li>
+                    <span className={"name"}>Hidayat Arghandabi</span>
+                    <span className={"phone"}>111111111111</span>
+                    <span className={"clearfix"}></span>
+                </li>
+                <li>
+                    <span className={"name"}>Ahmed Osman</span>
+                    <span className={"phone"}>111111111111</span>
+                    <span className={"clearfix"}></span>
+                </li>
+
+                
             </ul>
             </div>
           
