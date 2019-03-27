f71c5e61bbfccc3d75567a7ceb9efb9fb31b2f9f
Hidayat arghandabi <hidayatarg@gmail.com>
2019-03-27 16:09:36 +0300

    Setting the State

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,13 +2,40 @@ import React, { Component } from 'react';
 
 
 class Form extends Component {
+   constructor(){
+       super();
+    //    bind the method
+    this.onChange = this.onChange.bind(this);
+   }
+   
+    state = {
+        name: '',
+        phone: ''
+    };
+
+    onChange(event){
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
     render() {
         return (
             <div>
                 <form>
-                    <input name="name" id="name" placeholder="Enter a name" />
+                    <input 
+                    name="name" 
+                    id="name" 
+                    value={this.state.name} 
+                    onChange={this.onChange} 
+                    placeholder="Enter a name" />
                     <br/>
-                    <input name="phone" id="phone" placeholder="Enter a phone number" />
+                    <input 
+                    name="phone" 
+                    id="phone" 
+                    value={this.state.phone} 
+                    onChange={this.onChange} 
+                    placeholder="Enter a phone number" />
                     <button>Add</button>
                 </form>
             </div>
