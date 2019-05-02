ebe88a756314b7c0faf604c739d5289a7ef0628b
Hidayat Arghandabi <hidayatarg@gmail.com>
2019-05-02 18:50:01 +0300

    Added the build

---
+++ build/precache-manifest.50a17f10938c7ea0fd02dd90c72af25a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8dd91e91011e025eeeb6",
+    "url": "/static/js/main.d10c7222.chunk.js"
+  },
+  {
+    "revision": "4fd832ce7a6bc7a6c6b8",
+    "url": "/static/js/2.1739b0cf.chunk.js"
+  },
+  {
+    "revision": "8dd91e91011e025eeeb6",
+    "url": "/static/css/main.58d82868.chunk.css"
+  },
+  {
+    "revision": "11c89e91cd114053c5b3bfe7c19987c7",
+    "url": "/index.html"
+  }
+];
